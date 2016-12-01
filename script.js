3e2b23c9043c81560dba07110abfdcceb3c06596
webtech2k16 <s.n.khalil@student.tue.nl>
2016-12-01 18:28:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#button').click(function () {
         var toAdd = $('input[name=checkListItem]').val();
-        var bulletPoint = $('+');
+        var bulletPoint = document.createTextNode('+');
         $('.list').append('<div class="item">' + bulletPoint + toAdd + '</div>');
     });
     $(document).on('click', '.item', function () {
