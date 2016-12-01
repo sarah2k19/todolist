0acac3914411750c8e028fa7e3e2ebe85eb7cad8
webtech2k16 <s.n.khalil@student.tue.nl>
2016-12-01 16:43:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         $('.list').append('<div class="item">' + toAdd + '</div>');
     });
     $(document).(function () {
-        $('.list').on('click', 'item', function () {   
+        $(document).on('click', 'item', function () {   
             $(this).toggleClass("strike"); 
         });
     });
